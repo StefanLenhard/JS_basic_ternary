508bbfdb0119a6c563440fce96f7b941339f505c
Stefan Lenhard <stefan@stefanlenhard.com>
2023-10-25 17:40:13 +0200

    Fix: JS

--- javascript.js
+++ javascript.js
@@ -7,7 +7,8 @@ function update(bgColor, textColor) {
     html.style.color = textColor;
 }
 
-select.addEventListener("change";() => select.value === "black"
+select.addEventListener("change",() => select.value === "black"
     ? update("black", "white")
     : update("white", "black")
-)
+);
+
